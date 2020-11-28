e48f8ba4b8504f49f988b7ba17c0c00dad2cc46a
Yurii <streltsovyurii86@gmal.com>
2020-11-28 16:57:01 +0200

    Добавляю LightBox

--- src/js/searchAndViewImagesAPP.js
+++ src/js/searchAndViewImagesAPP.js
@@ -2,6 +2,7 @@ import ImageApiService from './apiService';
 import refs from './refs';
 import galleryTpl from '../templates/gallery.hbs';
 import LoadMoreBtn from './loadMoreBtn';
+import * as basicLightbox from 'basiclightbox';
 
 const loadMoreBtn = new LoadMoreBtn({
   selector: '[data-action="load-more"]',
@@ -30,7 +31,7 @@ function onSearch(event) {
 
 async function onLoadMore() {
   const scroll = document.documentElement.scrollHeight;
-    await fetchImages();
+  await fetchImages();
   onScroll(scroll);
 }
 
@@ -44,10 +45,26 @@ function appendHitsMarkup(hits) {
   refs.gallery.insertAdjacentHTML('beforeend', galleryTpl(hits));
 }
 
-function onScroll(scroll) { 
+function onScroll(scroll) {
   window.scrollTo({ top: scroll - 40, behavior: 'smooth' });
 }
- 
+
 function clearHitsContainer() {
   refs.gallery.innerHTML = '';
+}
+
+refs.gallery.addEventListener('click', openLightBox);
+
+function openLightBox(event) {
+  const set = { src: event.target.dataset.src, alt: event.target.alt };
+  openImage(set);
+}
+function openImage({ src, alt }) {
+  const instance = basicLightbox.create(`
+  <div class="full-image-container">
+  <img src="${src}" alt="${alt}" width="800" />
+  
+  </div>
+`);
+  instance.show();
 }
